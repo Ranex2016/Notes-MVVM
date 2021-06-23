1ddc0f9cc16961206452f3eb2d0886684fd6d271
Roma <Ranex@yandex.ru>
2021-06-23 15:26:13 +1000

    Инициализация объектов в MainActivity

--- app/src/main/java/com/example/notesmvvm/MainActivity.kt
+++ app/src/main/java/com/example/notesmvvm/MainActivity.kt
@@ -2,10 +2,33 @@ package com.example.notesmvvm
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import com.example.notesmvvm.databinding.ActivityMainBinding
+import com.example.notesmvvm.utilits.APP_ACTIVITY
 
 class MainActivity : AppCompatActivity() {
+    lateinit var toolbar: Toolbar
+    lateinit var navController:NavController
+    private var _binding: ActivityMainBinding? = null
+    val binding get() = _binding!!
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        APP_ACTIVITY = this
+        toolbar = binding.toolbar
+        navController = Navigation.findNavController(this,R.id.nav_host_fragment)
+        setSupportActionBar(toolbar)
+        title = getString(R.string.title)
+
     }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+
 }
---
+++ app/src/main/java/com/example/notesmvvm/utilits/constants.kt
@@ -0,0 +1,5 @@
+package com.example.notesmvvm.utilits
+
+import com.example.notesmvvm.MainActivity
+
+lateinit var APP_ACTIVITY:MainActivity
